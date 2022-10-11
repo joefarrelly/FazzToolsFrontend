c5a676455e0923da9677a07632110bb1e20f2e55
Joseph Farrelly <joefarrelly96@gmail.com>
2022-10-11 22:49:37 +0100

    test

--- src/App.js
+++ src/App.js
@@ -640,7 +640,7 @@ function Mount() {
           </div>
         </Col>
         <Col className="main-content">
-          <h2>Mount </h2>
+          <h2>Mount {data}</h2>
           <MountTable alts={data.slice(0, 10)} heads={heads} />
         </Col>
       </Row>
