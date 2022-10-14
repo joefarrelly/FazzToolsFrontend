8e0a6429794effd7b82625cb7a45042372384a45
Joseph Farrelly <joefarrelly96@gmail.com>
2022-10-14 01:05:30 +0100

    testing 9

--- src/App.js
+++ src/App.js
@@ -708,21 +708,21 @@ function Mount() {
 }
 
 function Pet() {
-  const [data, setData] = useState([]);
-  // const heads = []
+  // const [data, setData] = useState([]);
+  // // const heads = []
 
-  useEffect(() => {
-    async function getData() {
-      const response = await axios.get(config.url.API_URL + '/api/profile/userpets/', { params: { user: cookies.get('userid') }});
-      setData(response.data);
-    };
-    getData();
+  // useEffect(() => {
+  //   async function getData() {
+  //     const response = await axios.get(config.url.API_URL + '/api/profile/userpets/', { params: { user: cookies.get('userid') }});
+  //     setData(response.data);
+  //   };
+  //   getData();
 
-  }, []);
+  // }, []);
 
   // console.log(data);
   // console.log(data.slice(11, 14));
-  const count_data = data.slice(11, 14);
+  // const count_data = data.slice(11, 14);
   // console.log(count_data);
   // console.log(count_data[2]);
   return (
@@ -734,10 +734,10 @@ function Pet() {
             <MenuBar />
           </div>
         </Col>
-        <Col className="main-content">
+    {/*    <Col className="main-content">
           <h2>Pet {count_data[0]}/{count_data[2]}</h2>
-          {/*<PetTable alts={data.slice(0, 10)} heads={heads} />*/}
-        </Col>
+          {/*<PetTable alts={data.slice(0, 10)} heads={heads} />
+        </Col>*/}
       </Row>
     </>
   );
