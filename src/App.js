4280fd0eb748c0f4c7d357636aa3b468c50c5ee7
Joseph Farrelly <joefarrelly96@gmail.com>
2022-11-26 21:47:33 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -619,7 +619,7 @@ function Gear() {
 
   useEffect(() => {
     async function getData() {
-      const response = await axios.get(config.url.API_URL + '/api/profile/altequipments/', { params: { user: cookies.get('userid') }});
+      const response = await axios.get(config.url.API_URL + '/api/profile/altequipments/', { params: { user: cookies.get('userid'), fields: ['.altName', '.altRealm', '.get_altClass_display', 'head', 'neck', 'shoulder', 'back', 'chest', 'wrist', 'hands', 'belt', 'legs', 'feet', 'ring1', 'ring2', 'trinket1', 'trinket2', 'weapon1', 'weapon2'] }});
       setData(response.data);
     };
     getData();
